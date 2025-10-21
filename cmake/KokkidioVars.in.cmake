bf3b60983ccfb37f38af19f43180f02a344cd66f
lens <lennart.steffen@tu-berlin.de>
2025-10-21 13:19:25 +0200

    improved vectorisation support with CUDA (added arch=native, can be switched off with KOKKIDIO_NO_ARCH_NATIVE)

--- cmake/KokkidioVars.in.cmake
+++ cmake/KokkidioVars.in.cmake
@@ -3,5 +3,7 @@
 set(EIGEN_DIR "@Eigen3_DIR@")
 set(Kokkos_DIR "@Kokkos_DIR@")
 set(Kokkos_DEVICES "@Kokkos_DEVICES@")
+set(Kokkos_DEVICES "@Kokkos_DEVICES@")
 @OMP_RPATH_LINE@
+set(KOKKIDIO_NO_ARCH_NATIVE_CMAKE "@KOKKIDIO_NO_ARCH_NATIVE_CMAKE@")
 
